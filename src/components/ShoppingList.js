fc531bf2ca81a47a2809aecd0e6e709813bac04b
Anslie Brant <brantae@gmail.com>
2022-11-30 18:31:07 -0600

    commit tree f15d13ebacb506d201e20384320e0703b6e1acf5

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,39 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("")
+  
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  function handleFieldChange(event) {
+    //String.includes() method to determine whether one string may be found within another string, returning T or F
+    setSearch(event.target.value)
+
+  }
+
+  console.log(search)
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
+  }).filter((item) => {
+    return item.name.toLowerCase().includes(search.toLowerCase())
+
   });
+
+  
+
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleFieldChange} search={search} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
